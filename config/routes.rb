068bf43b51eed62edec58cfe670eb5ae044c9d38
t.akiya <rst.akiya@gmail.com>
2017-09-11 21:51:21 +0900

    Routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,36 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'customer#top'
+  namespace :admin do
+    resources :top do
+      collection do
+        post 'confirm'
+      end
+    end
+    resources :staff_member, only: [:new, :edit] do
+      collection do
+        post 'confirm'
+      end
+    end
+    get 'login', to: 'session#new', as: :login
+    post 'login', to: 'session#create'
+    delete 'logout', to: 'session#destroy', as: :logout
+  end
+
+  namespace :staff_member do
+    resources :top
+    get 'login', to: 'session#new', as: :login
+    post 'login', to: 'session#create'
+    delete 'logout', to: 'session#destroy', as: :logout
+  end
+
+  namespace :customer do
+    resources :top do
+      collection do
+        post 'confirm'
+      end
+    end
+    get 'login', to: 'session#new', as: :login
+    post 'login', to: 'session#create'
+    delete 'logout', to: 'session#destroy', as: :logout
+  end
 end
