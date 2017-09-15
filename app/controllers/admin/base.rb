445e4791707ebcc2e6f74e7b2704cb0efa2db192
t.akiya <rst.akiya@gmail.com>
2017-09-15 09:08:32 +0900

    Refactoring

--- app/controllers/admin/base.rb
+++ app/controllers/admin/base.rb
@@ -1,4 +1,7 @@
 class Admin::Base < ApplicationController
+  include ::Admin::SessionHelper
+  # これは全体のヘルパーになってしまうからヘルパーからインクルードしたほうがよい？
+  # helper_method :current_admin, :admin_logged_in?
   def log_in admin
     session[:admin_id] = admin.id
   end
@@ -7,11 +10,11 @@ class Admin::Base < ApplicationController
     session[:admin_id] = nil
   end
 
-  def current_admin
-    @current_admin ||= Admin.find_by(id: session[:admin_id])
-  end
-
-  def admin_logget_in?
-    !current_admin.nil?
-  end
+  # def current_admin
+  #   @current_admin ||= Admin.find_by(id: session[:admin_id])
+  # end
+  #
+  # def admin_logget_in?
+  #   !current_admin.nil?
+  # end
 end
--- app/helpers/admin/session_helper.rb
+++ app/helpers/admin/session_helper.rb
@@ -1,2 +1,9 @@
 module Admin::SessionHelper
+  def current_admin
+    @current_admin ||= Admin.find_by(id: session[:admin_id])
+  end
+
+  def admin_logget_in?
+    !current_admin.nil?
+  end
 end
