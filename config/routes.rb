abdc497f34f9d1bc0d7e8f9288adb83c6da7640c
t.akiya <rst.akiya@gmail.com>
2017-09-21 00:15:10 +0900

    Refactoring to routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,6 @@ Rails.application.routes.draw do
       collection do
         post 'confirm'
       end
-      get 'login', to: 'session#new', as: :login
-      post 'login', to: 'session#create'
-      delete 'logout', to: 'session#destroy', as: :logout
     end
 
     resources :staff_member do
@@ -17,6 +14,9 @@ Rails.application.routes.draw do
         post 'confirm'
       end
     end
+    get 'login', to: 'session#new', as: :login
+    post 'login', to: 'session#create'
+    delete 'logout', to: 'session#destroy', as: :logout
   end
 
   namespace :staff_member do
