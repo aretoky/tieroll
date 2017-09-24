9f333002fc823041dfd9fa82fb3da420455877c7
t.akiya <rst.akiya@gmail.com>
2017-09-24 16:44:23 +0900

    User action

--- app/controllers/customer/user_controller.rb
+++ app/controllers/customer/user_controller.rb
@@ -1,4 +1,5 @@
 class Customer::UserController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :destroy]
   def index
   end
 
@@ -6,12 +7,23 @@ class Customer::UserController < ApplicationController
   end
 
   def new
+    @user = User.new
   end
 
   def confirm
+    @user = User.new(user_params)
+    render :new, alert: "入力内容をかくにんしてね" if @user.invalid?
   end
 
   def create
+    @user = User.new(user_params)
+    if params[:back]
+      render :new
+    elsif @user.save
+      redirect_to :customer_root
+    else
+      render :new, alert: "入力内容を確認してね"
+    end
   end
 
   def edit
@@ -25,5 +37,10 @@ class Customer::UserController < ApplicationController
 
   private
   def user_params
-  end 
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  def set_user
+    @user = User.find_by(id: params[:user_id])
+  end
 end
