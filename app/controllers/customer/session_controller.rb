b79e68f1e66bc12f86fd4fb2ebd6a8010fbd590b
t.akiya <rst.akiya@gmail.com>
2017-09-24 18:57:48 +0900

    User

--- app/controllers/customer/session_controller.rb
+++ app/controllers/customer/session_controller.rb
@@ -3,8 +3,16 @@ class Customer::SessionController < ApplicationController
   end
 
   def create
+    user = user.find_by(email: params[:email])
+    if user
+      log_in user
+    else
+      render :new, alert: "入力内容を確認してね"
+    end
   end
 
   def destroy
-  end 
+    log_out
+    redirect_to :customer_root
+  end
 end
