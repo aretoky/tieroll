649390ab2e4c5a4a9773fa33175adecd96ea4ccc
t.akiya <rst.akiya@gmail.com>
2017-09-28 17:52:43 +0900

    User validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,13 @@
 class User < ApplicationRecord
   has_secure_password
-  validates :name, :email, presence: true
-  validates :name, length: {maximum: 15}
-  validates :email, uniqueness: true
-  validates :password, presence: true, allow_nil: true
+  # 開発中は面倒なので切っておく
+  # VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  # validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
+
+  validates :name, presence: true, length: {maximum: 15, minimum: 4}
+  validates :password, presence: true, allow_nil: true, length: {minimum: 6, maximum: 16}
   before_save{self.email = email.downcase}
+  
+  validates :email, uniqueness: true, presences: true
+
 end
