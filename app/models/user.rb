782dde11ee4071082ae6717826ca5f7adf6f9b3a
t.akiya <rst.akiya@gmail.com>
2017-09-28 17:56:11 +0900

    User validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
   # validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
 
   validates :name, presence: true, length: {maximum: 15, minimum: 4}
-  validates :password, presence: true, allow_nil: true, length: {minimum: 6, maximum: 16}
+  validates :password, presence: true, allow_nil: true#TODO,開発中はオフ length: {minimum: 6, maximum: 16}
   before_save{self.email = email.downcase}
 
   validates :email, uniqueness: true, presence: true
