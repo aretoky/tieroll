fd3510aaf42331d978d541dd0d34e25b5735c335
t.akiya <rst.akiya@gmail.com>
2017-10-01 14:10:47 +0900

    Staff Drawer

--- app/controllers/staff/member_controller.rb
+++ app/controllers/staff/member_controller.rb
@@ -1,17 +1,9 @@
 class Staff::MemberController < Staff::Base
-  before_action :are_you_staff?
-  before_action :set_staff, only: [:show, :edit, :update, :destroy]
+  before_action :are_you_staff?, only: [:show]
+  before_action :set_staff, only: [:show]
   def show
   end
 
-  def edit
-  end
-
-  def update
-  end
-
-  def destroy
-  end
   private
   def set_staff
     @staff = StaffMember.find_by(id: session[:staff_id])
