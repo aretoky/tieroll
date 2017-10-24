2e28c3beb81884718f2a732b90c4179386189e06
t.akiya <rst.akiya@gmail.com>
2017-10-24 10:10:03 +0900

    admin model test

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,11 +1,27 @@
 class Admin < ApplicationRecord
+  before_validation :email_down, :email_white_space
   has_secure_password
 
+
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  validates :name, presence: true, length: { minimum: 4, maximum: 16}
+
   validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
 
-  # validates :email, presence: true, uniqueness: true
-  
-  before_save{self.email = email.downcase}
-  validates :name, presence: true
+  validates :password, length: { minimun: 4, maximum: 16}
+
+
+  before_save{self.name = name.gsub(" ", "")}
+
+
+  protected
+  def email_down
+    self.email = email.downcase
+  end
+
+  def email_white_space
+    self.email = email.gsub(" ", "")
+  end
+
 end
--- spec/models/admin_spec.rb
+++ spec/models/admin_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Admin, type: :model do
   # pending "add some examples to (or delete) #{__FILE__}"
-  describe "admin create test" do
+  describe "create test" do
     it "正常に登録される" do
       admin = Admin.new(name: 'adminuser', email: 'admin@email.com', password: 'admin')
       expect(admin.save).to be_truthy
@@ -25,7 +25,7 @@ RSpec.describe Admin, type: :model do
   end
 
 
-  describe "admin email formt test" do
+  describe "email formt test" do
     it "@なしでは登録できない" do
       admin = Admin.new(name: 'adminuser', email: 'adminemail.com', password: 'admin')
       expect(admin.save).to be_falsey
@@ -42,4 +42,85 @@ RSpec.describe Admin, type: :model do
       expect(admin.save && admin2.save).to be_falsey
     end
   end
+
+
+  describe "name length test" do
+    it "３文字は保存されない" do
+      admin = Admin.new(name: '123', email: 'admin@email.com', password: 'admin')
+      expect(admin.save).to be_falsey
+    end
+
+    it "４文字は保存される" do
+      admin = Admin.new(name: '1234', email: 'admin@email.com', password: 'admin')
+      expect(admin.save).to be_truthy
+    end
+
+    it "16文字は保存される" do
+      admin = Admin.new(name: '1234512345123456', email: 'admin@email.com', password: 'admin')
+      expect(admin.save).to be_truthy
+    end
+
+    it "17文字は保存されない" do
+      admin = Admin.new(name: '12345123451234567', email: 'admin@email.com', password: 'admin')
+      expect(admin.save).to be_falsey
+    end
+  end
+
+
+  describe "空白処理" do
+    describe "nameの空白処理" do
+
+      it "先頭の空白を除去" do
+        admin = Admin.new(name: ' admin', email: 'admin@email.com', password: 'admin')
+        admin.save
+        expect(admin.name).to eq('admin')
+      end
+
+      it "末尾の空白を除去" do
+        admin = Admin.new(name: 'admin ', email: 'admin@email.com', password: 'admin')
+        admin.save
+        expect(admin.name).to eq('admin')
+      end
+
+      it "文字の間の空白を除去" do
+        admin = Admin.new(name: 'ad min', email: 'admin@email.com', password: 'admin')
+        admin.save
+        expect(admin.name).to eq('admin')
+      end
+
+      it "空白がどこにあっても除去" do
+        admin = Admin.new(name: ' ad m in ', email: 'admin@email.com', password: 'admin')
+        admin.save
+        expect(admin.name).to eq('admin')
+      end
+    end
+
+
+    describe "emailの空白処理" do
+      it "先頭の空白を除去" do
+        admin = Admin.new(name: 'admin', email: ' admin@email.com', password: 'admin')
+        admin.save
+        expect(admin.email).to eq('admin@email.com')
+      end
+
+      it "末尾の空白除去" do
+        admin = Admin.new(name: 'admin', email: 'admin@email.com ', password: 'admin')
+        admin.save
+        expect(admin.email).to eq('admin@email.com')
+      end
+
+      it "文字の間の空白を除去" do
+        admin = Admin.new(name: 'admin', email: 'admin@ email.com', password: 'admin')
+        admin.save
+        expect(admin.email).to eq('admin@email.com')
+      end
+
+      it "空白がどこにあっても除去" do
+        admin = Admin.new(name: 'admin', email: ' a dmin @ email.com ', password: 'admin')
+        admin.save
+        expect(admin.email).to eq('admin@email.com')
+      end
+    end
+  end   
+
 end
