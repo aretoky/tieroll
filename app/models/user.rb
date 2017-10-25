de81e920cc94d87891833669e4d05c9ada2cc8af
t.akiya <rst.akiya@gmail.com>
2017-10-25 10:34:41 +0900

    user model test

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,19 @@ class User < ApplicationRecord
 
   validates :name, presence: true, length: {maximum: 15, minimum: 4}
   validates :password, presence: true, allow_nil: true#TODO,開発中はオフ length: {minimum: 6, maximum: 16}
-  before_save{self.email = email.downcase}
+
+  before_save :email_white_space, :email_down
 
   # validates :email, uniqueness: true, presence: true
+
+  protected
+  def email_down
+    self.email = email.downcase
+  end
+
+  def email_white_space
+    self.email = email.strip
+    self.email = email.gsub(" ", "")
+  end
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,58 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+    describe "create" do
+      it "正常に登録される" do
+        user = User.new(name: 'user', email: 'user@email.com', password: 'user')
+        expect(user.save).to be_truthy
+      end
+
+      it "名前が空だと保存されない" do
+        user = User.new(name: '', email: 'user@email.com', password: 'user')
+        expect(user.save).to be_falsey
+      end
+
+      it "emailが空でも保存されない" do
+        user = User.new(name: 'user', email: '', password: 'user')
+        expect(user.save).to be_falsey
+      end
+
+      it "passwordが空なんてもってのほか" do
+        user = User.new(name: 'user', email: 'user@email.com', password: '')
+        expect(user.save).to be_falsey
+      end
+    end
+
+
+    describe "email format" do
+      it "@なしでは登録されない" do
+        user = User.new(name: 'user', email: 'useremail.com', password: 'user')
+        expect(user.save).to be_falsey
+      end
+
+      it "@以降に.が無いとダメ" do
+        user = User.new(name: 'user', email: 'user@emailcom', password: 'user')
+        expect(user.save).to be_falsey
+      end
+
+      it "@の後に.だけでもだめ" do
+        user = User.new(name: 'user', email: 'user@.', password: 'user')
+        expect(user.save).to be_falsey
+      end
+
+      it "@の直後に.stringだけでもだめ" do
+        user = User.new(name: 'user', email: 'user@.com', password: 'user')
+        expect(user.save).to be_falsey
+      end
+    end
+
+    describe "重複" do
+      describe "email" do
+        it "重複は認めない" do
+          user = User.new(name: 'user', email: 'user@email.com', password: 'user')
+          user2 = User.new(name: 'user', email: 'user@email.com', password: 'user')
+          expect(user.save && user2.save).to be_falsey
+        end
+      end
+    end
 end
