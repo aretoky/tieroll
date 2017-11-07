0dbb4ee96ae298a4a909b3e54584cd0a1dfc2399
t.akiya <rst.akiya@gmail.com>
2017-11-07 12:51:00 +0900

    shoes create

--- app/controllers/staff/shoes_controller.rb
+++ app/controllers/staff/shoes_controller.rb
@@ -5,12 +5,21 @@ class Staff::ShoesController < Staff::Base
   end
 
   def new
+    @shoes = Shoe.new
   end
 
   def confirm
   end
 
   def create
+    @shoes = @current_staff.shoes.build(shoes_params)
+    if params[:back]
+      render :new, notice: "編集してね"
+    elsif @shoes && @shoes.save
+      redirect_to :staff_item_post, notice: "登録したよ"
+    else
+      render :new, alert: "確認してね"
+    end
   end
 
   def edit
@@ -24,6 +33,6 @@ class Staff::ShoesController < Staff::Base
 
   private
   def shoes_params
-    params.require(:shoes).permit(:price, :size, :color, :pattern, :season, :scene, :description, :raw_materials, :shoes_name, :shoes_code, :shoes_one, :shoes_two, :shoes_three, :shoes_four, :shoes_five)
+    params.require(:shoe).permit(:price, :size, :color, :pattern, :season, :scene, :description, :raw_materials, :shoes_name, :shoes_code, :shoes_one, :shoes_two, :shoes_three, :shoes_four, :shoes_five)
   end
 end
