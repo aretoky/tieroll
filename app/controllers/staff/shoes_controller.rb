5f4b6e7507d581687a456eb805a527b33f1c2c8f
t.akiya <rst.akiya@gmail.com>
2017-11-18 02:21:09 +0900

    shoes edit

--- app/controllers/staff/shoes_controller.rb
+++ app/controllers/staff/shoes_controller.rb
@@ -1,7 +1,10 @@
 class Staff::ShoesController < Staff::Base
   before_action :are_you_staff_member?, only: [:index, :new, :create, :show, :edit, :update, :destroy]
 
+  before_action :set_shoes, only: %i(show edit update destroy)
+
   def index
+    @shoes = Shoe.where(staff_member: @current_staff.id)
   end
 
   def new
@@ -26,6 +29,13 @@ class Staff::ShoesController < Staff::Base
   end
 
   def update
+    if @shoes.invalid?
+      render :new, alert: '確認してね'
+    elsif @shoes.update!(shoes_params)
+      redirect_to :staff_shoes, notice: '完了'
+    else
+      render :new, alert: '確認してね'
+    end
   end
 
   def destroy
@@ -34,5 +44,9 @@ class Staff::ShoesController < Staff::Base
   private
   def shoes_params
     params.require(:shoe).permit(:price, :size, :color, :pattern, :season, :scene, :description, :raw_materials, :shoe_name, :shoe_code, :shoe_front, :shoe_back, :shoe_inner, :shoe_out_side, :shoe_sole, :toe, :shoe_one, :shoe_two, :shoe_three, :shoe_four, :shoe_five)
+  end
+
+  def set_shoes
+    @shoes = Shoe.find_by(id: params[:id])
   end
 end
