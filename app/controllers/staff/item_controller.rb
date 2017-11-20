773cda77659e44cb0d916c3f4fa441090576493a
t.akiya <rst.akiya@gmail.com>
2017-11-21 04:41:12 +0900

    sorted item list

--- app/controllers/staff/item_controller.rb
+++ app/controllers/staff/item_controller.rb
@@ -1,12 +1,12 @@
 class Staff::ItemController < Staff::Base
   before_action :are_you_staff_member?, only: %i(item_edit search_of_item)
   include HowModel
-  
+
   def item_post
   end
 
   def item_edit
-    @how = {ベルト: "belt", コート: "coat", カフリンクス: "cuff_link", イヤーマフ: "ear_muffler", 手袋: "glove", ハット: "hat", ジャケット: "jacket", 'ニット/セーター': "knit", ラペルピン: "lapel_pin", マフラー: "muffler", パンツ: "pant", チーフ: "pocket_chief", 靴: "shoe", シャツ: "shurt", 靴下: "sock", ネクタイ: "tie", タイピン: "tie_pin", ベスト: "vest", その他: "other"}
+    @how = {帽子: "hat", イヤーマフラー: "ear_muffler", ネクタイ: "tie", マフラー: "muffler", タイピン: "tie_pin", シャツ: "shurt", 'ニット/セーター': "knit", ベスト: "vest", ジャケット: "jacket", コート: "coat", チーフ: "pocket_chief", カフリンクス: "cuff_link", ラペルピン: "lapel_pin", 手袋: "glove", パンツ: "pant", ベルト: "belt", 靴下: "sock", 靴: "shoe", その他: "other"}
   end
 
   def search_of_item
