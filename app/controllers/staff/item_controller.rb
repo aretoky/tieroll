f057cbcd861886223823ce1ef6e6a140074c7fca
t.akiya <rst.akiya@gmail.com>
2017-11-21 10:40:28 +0900

    speling mistake

--- app/controllers/staff/item_controller.rb
+++ app/controllers/staff/item_controller.rb
@@ -11,7 +11,7 @@ class Staff::ItemController < Staff::Base
 
   def search_of_item
     # binding.pry
-    # @item = select_model.where(staff_member: @current_staff.id, code: params[:product_code])
+    # item = select_model.where(staff_member: @current_staff.id, code: params[:product_code])
     # ↑の方法だと一発で取得できるがインスタンスの型がActiveRecord_Relationになっていて、編集フォームに渡すのに不都合だと思うから少し遅くなりそうだけど↓にしてみる？
     items = select_model.where(staff_member: @current_staff.id)
     item = items.find_by(code: params[:product_code])
