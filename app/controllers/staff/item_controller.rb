dd85e01044fbf828f0aecd88ae5f8e99ea178406
t.akiya <rst.akiya@gmail.com>
2017-11-21 10:37:23 +0900

    search form

--- app/controllers/staff/item_controller.rb
+++ app/controllers/staff/item_controller.rb
@@ -11,7 +11,11 @@ class Staff::ItemController < Staff::Base
 
   def search_of_item
     # binding.pry
-    item = params[:item]
-    @item = select_model.where(staff_member: @current_staff.id, code: params[:product_code])
+    # @item = select_model.where(staff_member: @current_staff.id, code: params[:product_code])
+    # ↑の方法だと一発で取得できるがインスタンスの型がActiveRecord_Relationになっていて、編集フォームに渡すのに不都合だと思うから少し遅くなりそうだけど↓にしてみる？
+    items = select_model.where(staff_member: @current_staff.id)
+    item = items.find_by(code: params[:product_code])
+    # binding.pry
+    redirect_to controller: params[:item], action: 'edit', id: item.id
   end
 end
