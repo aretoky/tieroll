14e6c321d84830019ce9194f8ce5059926dcf521
t.akiya <rst.akiya@gmail.com>
2017-11-27 18:10:34 +0900

    README

--- app/controllers/concerns/search_item.rb
+++ app/controllers/concerns/search_item.rb
@@ -1,45 +1,71 @@
 module SearchItem
   extend ActiveSupport::Concern
+  # input_any_itemsを呼ぶときは先にset_required_itemsを呼ぶ事
 
   # シャツ、パンツ、靴下、靴は必ず存在する
-  def set_primary_items
-    @shurt = Shurt.where(staff_member: @current_staff.id, code: params[:marchandise][:shurt_code]).pluck(:id, :price, :s_front)
-    @pant = Pant.where(staff_member: @current_staff.id, code: params[:marchandise][:pant_code]).pluck(:id, :price, :p_side)
-    @socks = Sock.where(staff_member: @current_staff.id, code: params[:marchandise][:sock_code]).pluck(:id, :price, :socks_one)
-    @shoes = Shoe.where(staff_member: @current_staff.id, code: params[:marchandise][:shoe_code]).pluck(:id, :price, :shoe_front)
+  def set_required_items
+    @shurt = Shurt.where(staff_member: @current_staff.id, code: params[:marchandise][:shurt_code]).pluck(:id, :s_front, :code)
+    @pant = Pant.where(staff_member: @current_staff.id, code: params[:marchandise][:pant_code])
+    @socks = Sock.where(staff_member: @current_staff.id, code: params[:marchandise][:sock_code])
+    @shoes = Shoe.where(staff_member: @current_staff.id, code: params[:marchandise][:shoe_code])
   end
 
   # 入力があれば引っ張ってくる
-  # 全てにおいてインスタンス = whereで現在のスタッフ且つ該当CODEを持つもの.pluck(:id, :price, )　if marchandiseにアイテムがあれば　の形
-  def set_items
-    @hat = Hat.where(staff_member: @current_staff.id, code: params[:marchandise][:hat_code]).pluck(:id, :price, :hat_front) if params[:marchandise][:hat_code]
+  def set_any_items
+    @hat = Hat.where(staff_member: @current_staff.id, code: params[:marchandise][:hat_code]) if params[:marchandise][:hat_code]
 
-    @ear_muffler = EarMuffler.where(staff_member: @current_staff.id, code: params[:marchandise][:ear_muffler_code]).pluck(:id, :price, :ear_muffler_front) if params[:marchandise][:ear_muffler_code]
+    @ear_muffler = EarMuffler.where(staff_member: @current_staff.id, code: params[:marchandise][:ear_muffler_code]) if params[:marchandise][:ear_muffler_code]
 
-    @tie = TieNeck.where(staff_member: @current_staff.id, code: params[:marchandise][:tie_code]).pluck(:id, :price, :tie_one) if params[:marchandise][:tie_code]
+    @tie = TieNeck.where(staff_member: @current_staff.id, code: params[:marchandise][:tie_code]) if params[:marchandise][:tie_code]
 
-    @muffler = Muffler.where(staff_member: @current_staff.id, code: params[:marchandise][:muffler_code]).pluck(:id, :price, :muffler_front) if params[:marchandise][:muffler_code]
+    @muffler = Muffler.where(staff_member: @current_staff.id, code: params[:marchandise][:muffler_code]) if params[:marchandise][:muffler_code]
 
-    @tie_pin = TiePin.where(staff_member: @current_staff.id, code: params[:marchandise][:tie_pin_code]).pluck(:id, :price, :tie_pin_one) if params[:marchandise][:tie_pin_code]
+    @tie_pin = TiePin.where(staff_member: @current_staff.id, code: params[:marchandise][:tie_pin_code]) if params[:marchandise][:tie_pin_code]
 
-    @knit = Knit.where(staff_member: @current_staff.id, code: params[:marchandise][:knit_code]).pluck(:id, :price, :knit_front) if params[:marchandise][:knit_code]
+    @knit = Knit.where(staff_member: @current_staff.id, code: params[:marchandise][:knit_code]) if params[:marchandise][:knit_code]
 
-    @vest = Vest.where(staff_member: @current_staff.id, code: params[:marchandise][:vest_code]).pluck(:id, :price, :v_front) if params[:marchandise][:vest_code]
+    @vest = Vest.where(staff_member: @current_staff.id, code: params[:marchandise][:vest_code]) if params[:marchandise][:vest_code]
 
-    @jacket = Jacket.where(staff_member: @current_staff.id, code: params[:marchandise][:jacket_code]).pluck(:id, :price, :j_front) if params[:marchandise][:jacket_code]
+    @jacket = Jacket.where(staff_member: @current_staff.id, code: params[:marchandise][:jacket_code]) if params[:marchandise][:jacket_code]
 
-    @coat = Coat.where(staff_member: @current_staff.id, code: params[:marchandise][:coat_code]).pluck(:id, :price, :coat_front) if params[:marchandise][:coat_code]
+    @coat = Coat.where(staff_member: @current_staff.id, code: params[:marchandise][:coat_code]) if params[:marchandise][:coat_code]
 
-    @chief = Pockethief.where(staff_member: @current_staff.id, code: params[:marchandise][:chief_code]).pluck(:id, :price, :chief_one) if params[:marchandise][:chief_code]
+    @chief = PocketChief.where(staff_member: @current_staff.id, code: params[:marchandise][:chief_code]) if params[:marchandise][:chief_code]
 
     @cuff = CuffLink.where(staff_member: @current_staff.id, code: params[:marchandise][:cuff_link_code]).pluck(:id, :price, :cuff_link_one) if params[:marchandise][:cuff_link_code]
 
-    @lapel_pin = LapelPin.where(staff_member: @current_staff.id, code: params[:marchandise][:lapel_pin_code]).pluck(:id, :price, :lapel_pin_one) if params[:marchandise][:lapel_pin_code]
+    @lapel_pin = LapelPin.where(staff_member: @current_staff.id, code: params[:marchandise][:lapel_pin_code]) if params[:marchandise][:lapel_pin_code]
 
-    @glove = Gant.where(staff_member: @current_staff.id, code: params[:marchandise][:glove_code]).pluck(:id, :price, :glove_front) if params[:marchandise][:glove_code]
+    @glove = Gant.where(staff_member: @current_staff.id, code: params[:marchandise][:glove_code]) if params[:marchandise][:glove_code]
 
-    @belt = Belt.where(staff_member: @current_staff.id, code: params[:marchandise][:belt_code]).pluck(:id, :price, :belt_one) if params[:marchandise][:belt_code]
+    @belt = Belt.where(staff_member: @current_staff.id, code: params[:marchandise][:belt_code]) if params[:marchandise][:belt_code]
 
-    @other = Other.where(staff_member: @current_staff.id, code: params[:marchandise][:other_code]).pluck(:id, :price, :other_one) if params[:marchandise][:other_code]
+    @other = Other.where(staff_member: @current_staff.id, code: params[:marchandise][:other_code]) if params[:marchandise][:other_code]
+  end
+
+  #TODO もっと簡単に書けそう
+  def input_items
+    # シャツ、パンツ、靴下、靴は必ずあるものとする
+    # 先に↑のset_required_itemsを呼ぶこと
+    @marchandise.shurt = @shurt.id
+    @marchandise.pant = @pant.id
+    @marchandise.sock = @socks.id
+    @marchandise.shoe = @shoes.id
+
+    @marchandise.hat = @hat.id if @hat
+    @marchandise.ear_muffler = @ear_muffler.id if @ear_muffler
+    @marchandise.tie_neck = @tie.id if @tie
+    @marchandise.muffler = @muffler.id if @muffler
+    @marchandise.tie_pin = @tie_pin.id if @tie_pin
+    @marchandise.knit = @knit.id if @knit
+    @marchandise.vest = @vest.id if @vest
+    @marchandise.jacket = @jacket.id if @jacket
+    @marchandise.coat = @coat.id if @coat
+    @marchandise.pocket_chief = @chief if @chief
+    @marchandise.cuff_link = @cuff.id if @cuff
+    @marchandise.lapel_pin = @lapel_pin.id if @lapel_pin
+    @marchandise.gant = @glove.id if @glove
+    @marchandise.belt = @belt.id if @belt
+    @marchandise.other = @other.id if @other
   end
 end
--- app/controllers/staff/marchandise_controller.rb
+++ app/controllers/staff/marchandise_controller.rb
@@ -2,13 +2,30 @@ class Staff::MarchandiseController < Staff::Base
   include SearchItem #Concern
 
   before_action :are_you_staff_member?, only: %i(index new confirm create show edit update destroy)
-  before_action :set_marchan, only: %i(new edit)
+  before_action :set_marchandise_label, only: %i(new edit)
 
   def new
     @marchandise = Marchandise.new
   end
 
   def confirm
+    @marchandise = @current_staff.marchandises.build(marchandise_params)
+    # set_required_items #SearchItem
+    # set_any_items #SearchItem
+    # input_items #SearchItem
+    if params[:marchandise][:shurt_code]
+      # shurts = Shurt.where(staff_member: @current_staff.id)
+      # shurt = shurts.find_by(code: params[:marchandise][:shurt_code])
+      shurt = Shurt.where(staff_member: @current_staff.id, code: params[:marchandise][:shurt_code]).pluck(:id, :s_front, :code).flatten!
+      # binding.pry
+    end
+
+    # shurt.each do |f|
+    #   binding.pry
+    # end
+    @marchandise.shurt = shurt.id
+    # binding.pry
+
     render :new, alert: "編集してね" if @marchandise.invalid?
   end
 
@@ -36,11 +53,12 @@ class Staff::MarchandiseController < Staff::Base
 
   private
   def marchandise_params
-    params.require(:main_product).permit(:product_face, :code, :product_scene, :season, :description)
+    params.require(:marchandise).permit(:product_face, :code, :product_scene, :season, :description, :shurt)
   end
 
-  def set_marchan
+  def set_marchandise_label
     @season = Season.pluck(:product_season, :id)
     @scene = ProductScene.pluck(:marchandise_scene, :id)
   end
+
 end
