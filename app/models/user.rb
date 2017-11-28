de06f0ce42ddeb93b59e8c4c522fd667a06d382e
t.akiya <rst.akiya@gmail.com>
2017-11-28 19:53:23 +0900

    email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ApplicationRecord
+  include EmailExchange
+
+  before_validation do
+    self.email = normalize_as_email(email) if email
+  end
+
+
   has_secure_password
   # 開発中は面倒なので切っておく
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -7,18 +14,16 @@ class User < ApplicationRecord
   validates :name, presence: true, length: {maximum: 15, minimum: 4}
   validates :password, presence: true, allow_nil: true#TODO,開発中はオフ length: {minimum: 6, maximum: 16}
 
-  before_save :email_white_space, :email_down
-
   # validates :email, uniqueness: true, presence: true
 
   protected
-  def email_down
-    self.email = email.downcase
-  end
+  # def email_down
+  #   self.email = email.downcase
+  # end
 
-  def email_white_space
-    self.email = email.strip
-    self.email = email.gsub(" ", "")
-  end
+  # def email_white_space
+  #   self.email = email.strip
+  #   self.email = email.gsub(" ", "")
+  # end
 
 end
