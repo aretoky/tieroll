60491cf63271230f356c2bab28456af133441695
t.akiya <rst.akiya@gmail.com>
2017-11-29 14:03:46 +0900

    marchandise create

--- app/controllers/staff/marchandise_controller.rb
+++ app/controllers/staff/marchandise_controller.rb
@@ -23,18 +23,27 @@ class Staff::MarchandiseController < Staff::Base
     # else
     #   render :new, alert: "確認してね"
     # end
-    elsif @marchandise
+    elsif @marchandise #marchandiseがあれば必須アイテムをセットし、その他アイテムをセットし、@marchandiseにアイテムの値段の合計を代入してさらにアイテムのIDを代入する
       if set_required_items
         set_any_items
         @marchandise.price = set_item_price
         input_items
         binding.pry
-        render :new
-      end
-    else
-      binding.pry
 
-      render :new
+        if @marchandise.save! #@marchandiseがあり、その他の入力全ての処理が正常に終了したら
+          redirect_to :staff_item_post, notice: '登録完了'
+        else
+          render :new, alert: '確認してね'
+        end
+
+      else  #@marchandiseは存在するが各種カラムの代入が正常に行われなかった時
+        render :new, alert: '確認してね'
+      end 
+
+    else #なにはともあれFalseのとき
+        binding.pry
+
+        render :new
     end
 
   end
--- app/models/marchandise.rb
+++ app/models/marchandise.rb
@@ -1,28 +1,27 @@
 class Marchandise < ApplicationRecord
-  validates :staff_member, :product_face, :code, :season, :scene, :description, :price, :shurt, :pant, :sock, :shoe, presence: true
-  # validates :staff_member, presence: true
+  validates :staff_member, :product_face, :code, :season, :product_scene, :description, :price, :shurt, :pant, :sock, :shoe, presence: true
 
 
   belongs_to :staff_member
-  belongs_to :belt
-  belongs_to :jacket
+  belongs_to :belt,         optional: true
+  belongs_to :jacket,       optional: true
   belongs_to :pant
-  belongs_to :pocket_chief
+  belongs_to :pocket_chief, optional: true
   belongs_to :shoe
   belongs_to :shurt
-  belongs_to :tie_pin
+  belongs_to :tie_pin,      optional: true
   belongs_to :sock
-  belongs_to :tie_neck
-  belongs_to :vest
-  belongs_to :coat
-  belongs_to :muffler
-  belongs_to :ear_muffler
-  belongs_to :gant
-  belongs_to :knit
-  belongs_to :cuff_link
-  belongs_to :lapel_pin
-  belongs_to :hat
-  belongs_to :other
+  belongs_to :tie_neck,     optional: true
+  belongs_to :vest,         optional: true
+  belongs_to :coat,         optional: true
+  belongs_to :muffler,      optional: true
+  belongs_to :ear_muffler,  optional: true
+  belongs_to :gant,         optional: true
+  belongs_to :knit,         optional: true
+  belongs_to :cuff_link,    optional: true
+  belongs_to :lapel_pin,    optional: true
+  belongs_to :hat,          optional: true
+  belongs_to :other,        optional: true
 
   mount_uploader :product_face, ProductFaceUploader
   # PRODUCT_SCENE = {'ビジネス': 'Business', 'ビジカジ': 'Office_casual', '休日': 'Holiday', 'デート': 'Date', 'パーティー/お呼ばれ': 'Party' '結婚式': 'Wedding'}
